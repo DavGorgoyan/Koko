bdc644b2fddd259bd7734e9d45d25ea4cac5b2bc
Dav Gorgoyan <gorgoyandav@gmail.com>
2022-08-20 16:06:45 +0400

    delete odd router in admin and bring together

--- src/router/admin/admin.js
+++ src/router/admin/admin.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import newsRouter from "./news.js"
 import productRouter from "./product.js"
-import transactionRouter from "./transactions.js"
+import { getTransactionsController } from "../../controllers/admin/transactions.js"
 const router = Router();
 
 router.use("/news", newsRouter);
 router.use("/product", productRouter);
-router.use("/trans", transactionRouter);
+router.get("/trans", getTransactionsController);
 
 
 
--- src/router/admin/transactions.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getTransactionsController } from "../../controllers/admin/transactions.js";
-const router = Router();
-
-router.get("", getTransactionsController)
-
-
-
-export default router;
